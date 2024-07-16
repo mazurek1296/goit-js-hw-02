7e727375f625abf7d3feb2eddd0d1d2adf76652c
mazurek1296 <165072430+mazurek1296@users.noreply.github.com>
2024-07-16 18:25:45 +0200

    Add files via upload

--- task-1.js
+++ task-1.js
@@ -1,9 +1,10 @@
-makeTransaction(quantity, pricePerDroid, customerCredits);
-const totalPrice = quantity * pricePerDroid;
-if (totalPrice > customerCredits) {
-  return "Insufficient funds!";
-} else {
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
